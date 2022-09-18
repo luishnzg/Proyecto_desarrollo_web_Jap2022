7c5b7f8c45fc8b5a52727dd185842adc6ccb62d7
luishnzg <luismhnzg@gmail.com>
2022-09-18 19:30:59 -0300

    final changes entregable 3

--- js/product-info.js
+++ js/product-info.js
@@ -1,16 +1,18 @@
 let productsID = localStorage.getItem("productID");
 let productInfo;
 let productComments = [];
+let newObj = {};
+
 function printImages(x) {
-    let htmlProducInfoContentAppendTittle =  `<div> <h4> Imagenes ilustrativas </h4></div> <br>`;
+    
     let htmlProducInfoContentAppend = "";
     for (let i = 0; i < x.images.length; i++) {
         let array = x;
         htmlProducInfoContentAppend += 
-        `<img src="${array.images[i]}" alt="${array.description}" class="img-thumbnail col-3">`;
+        `<img src="${array.images[i]}" alt="${array.description}" class="img-thumbnail  col-3">`;
         
     }
-    document.getElementById("containerProductInfoImages").innerHTML = htmlProducInfoContentAppendTittle +  htmlProducInfoContentAppend;
+    document.getElementById("containerProductInfoImages").innerHTML = htmlProducInfoContentAppend;
 
 };
 function showProductInfo(x) {
@@ -18,7 +20,7 @@ function showProductInfo(x) {
    let htmlProducInfoContentAppend =
       `
       <div>
-      <div style="margin:50px">
+      <div class="mt-4 mb-4">
             <h2>${array.name}
             </h2>
         </div>
@@ -44,7 +46,8 @@ for (let i = 0; i < x.length; i++) {
     let array = x[i];
     htmlProducInfoContentAppend += 
     `
-    <div id=${array.product}>
+    <div class="list-group-item list-group-item-action" id=${array.product}>  
+    <p><span style="font-weight:bold">${array.user}</span> - ${array.dateTime} - <span class="fa fa-star checked"> ${array.score} </span></p>
     <p>${array.description} </p>
     </div>
 
@@ -56,12 +59,6 @@ for (let i = 0; i < x.length; i++) {
 
 };
 
-/*"product": 50921,
-"score": 3,
-"description": "Ya llevo un año con este auto y la verdad que tiene sus ventajas y desventajas",
-"user": "juan_pedro",
-"dateTime": "2020-02-25 18:03:52"
-},*/
 
 document.addEventListener("DOMContentLoaded", function () {
     getJSONData(PRODUCT_INFO_URL + productsID + EXT_TYPE).then(function (resultObj) {
@@ -71,12 +68,36 @@ document.addEventListener("DOMContentLoaded", function () {
             printImages(productInfo);
 
         }
-    })
+    });
     getJSONData(PRODUCT_INFO_COMMENTS_URL + productsID + EXT_TYPE).then(function (resultObj) {
         if (resultObj.status === "ok") {
             productComments = resultObj.data;
             printComments(productComments);
         }
+    });
+     //Identifica el elemento con un ID especifico para inyectarle codigo HTML mas la informacion que esta en el localstorage
+     document.getElementById("usuarioNavProductsInfo").innerHTML = `<a class="nav-link" href="" >${localStorage.getItem('usuario')}</a>`;
+
+    document.getElementById("productInfoCommentBtn").addEventListener("click", function (){
+        
+        let options = {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'}
+        let date = new Date().toLocaleDateString("sv-SE", options);
+        newObj.product = Number(localStorage.getItem('productID'));
+        newObj.score = Number(document.getElementById("scoreListInput").value);
+        newObj.description = document.getElementById("opinion").value;
+        newObj.user = localStorage.getItem('usuario');
+        newObj.dateTime = date;
+        productComments.push(newObj);
+        printComments(productComments);
+        document.getElementById("opinion").value = "";
+        document.getElementById("scoreListInput").value = "";
+        delete newObj.product;
+        delete newObj.score;
+        delete newObj.description;
+        delete newObj.user;
+        delete newObj.dateTime;
+
     })
+
 })
 
